ae0249035409ed4c4df0f2f40bbf176055cedd25
Alex Leontiev <alozz1991@gmall.com>
2018-11-04 16:50:01 +0900

    commit

--- TrelloAssistant.java
+++ TrelloAssistant.java
@@ -39,28 +39,22 @@ public class TrelloAssistant {
 		return res;
 	}
 	public void setCardDuedone(String cardid,boolean duedone) throws Exception {
-//		HttpPut put = new HttpPut(String.format("https://api.trello.com/1/cards/%s?key=%s&token=%s&dueComplete=%s", cardid,key_,token_,duedone?"true":"false"));
 		String uri = String.format("https://api.trello.com/1/cards/%s?key=%s&token=%s&dueComplete=%s", 
 				cardid,key_,token_,duedone?"true":"false"); 
 		HttpString(uri,client_,false,HTTPMETHOD.PUT);
-//		CloseableHttpResponse chr = client_.execute(put);
-//		chr.close();
 	}
 	public void archiveCard(String cardid) throws Exception {
 		String uri = String.format("https://api.trello.com/1/cards/%s?key=%s&token=%s&closed=true", cardid,key_,token_);
-//        Util.PutString(uri,client_,true);
 		HttpString(uri,client_,true,HTTPMETHOD.PUT);
 	}
 	public void setLabel(String cardid, String labelColor) throws Exception {
 		System.out.println(String.format("cardid=%s, label=%s", cardid,labelColor));
 		String uri = String.format("https://api.trello.com/1/cards/%s/labels?key=%s&token=%s&color=%s&name=failed", cardid,key_,token_,labelColor);
-//        Util.PostString(uri,client_,true);
 		HttpString(uri,client_,true,HTTPMETHOD.POST);
 	}
 	public void moveCard(String cardid, String newListId) throws Exception {
 		System.out.println(String.format("cardid=%s, newListId=%s", cardid,newListId));
 		String uri = String.format("https://api.trello.com/1/cards/%s?key=%s&token=%s&idList=%s", cardid,key_,token_,newListId);
-//        Util.PutString(uri,client_,true);
 		HttpString(uri,client_,true,HTTPMETHOD.PUT);
 	}
 	public String findListByName(String boardId,String listName) throws Exception {
@@ -78,8 +72,9 @@ public class TrelloAssistant {
 		throw new Exception(String.format("no such list: %s", listName));
 	}
 	/*so far we support:
-	 * 	name
-	 * 	due
+	 * 	name : String
+	 * 	due : Date
+	 * checklist : JSONArray
 	 */
 	public JSONObject addCard(String idList,JSONObject card) throws Exception {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
@@ -91,12 +86,48 @@ public class TrelloAssistant {
 				URLEncoder.encode(card.getString("name")),
 				card.has("due")?("&due="+URLEncoder.encode(dateFormat.format(((Date)card.get("due"))))):"");
 		String reply = HttpString(uri,client_,true,HTTPMETHOD.POST);
-		return new JSONObject(reply);
+		JSONObject res = new JSONObject(reply);
+		if(card.has("checklist")) {
+			JSONArray checklist = card.getJSONArray("checklist");
+			String checklistName = (String)checklist.remove(0);
+			String id = res.getString("id");
+			addCheckList(id,checklistName,checklist);
+		}
+		
+		return res;
+	}
+	void addCheckList(String cardId,String checklistName,JSONArray checklist) throws Exception {
+		String uri = String.format("%s?key=%s&token=%s&idCard=%s&name=%s",
+				"https://api.trello.com/1/checklists",
+				key_,
+				token_,
+				cardId,
+				checklistName
+		);
+		String reply = HttpString(uri,client_,true,HTTPMETHOD.POST);
+		JSONObject obj = new JSONObject(reply);
+		System.err.format("got: %s\n", obj.toString(2));
+		for(int i = 0; i < checklist.length(); i++) {
+			String item = checklist.getString(i);
+			addCheckListItem(obj.getString("id"),item);
+		}
+	}
+	void addCheckListItem(String checkListId,String itemName) throws Exception {
+		String uri = String.format("%s/%s/checkItems?key=%s&token=%s&name=%s&checked=false&pos=bottom",
+//				"https://api.trello.com/1/checklists",
+				"https://api.trello.com/1/checklists/",
+				checkListId,
+				key_,
+				token_,
+//				cardId,
+//				checklistName
+				itemName
+		);
+		String reply = HttpString(uri,client_,true,HTTPMETHOD.POST);
+		System.err.format("%s=>\n%s\n", itemName,reply);
 	}
 	public void removeCard(String id) throws Exception {
-		//https://api.trello.com/1/cards/id
 		String uri = String.format("https://api.trello.com/1/cards/%s?key=%s&token=%s",id,key_,token_);
-//		Util.DeleteString(uri,client_,true);
 		HttpString(uri,client_,true,HTTPMETHOD.DELETE);
 	}
 	public JSONArray getListActions(String listId, JSONObject obj) throws Exception {
